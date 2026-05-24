09acbbadb58f90a4812772fd770e6d758166c24e
leroimerlin1 <zakdjbr.shopify@gmail.com>
2026-05-24 02:24:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,8 @@ from telegram.ext import (
 # CONFIGURATION
 # =============================================================
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN = "8744963419:AAFcngIdV_pF3pITHbAOiydfFRlO5Tl0qCc"
+
 
 
 CHANNEL_LINK = "https://t.me/+xkLrkV6xQBQ2OTQ0"
